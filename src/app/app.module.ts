f2adae63674131026ac6d80f06cd407b9ca009bb
Almira Shaikh <almira.shaikh@1Rintern.local>
2022-10-12 14:36:33 +0530

    feat: folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { CompanyPipe } from './shared/pipe/company.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompanyPipe,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
