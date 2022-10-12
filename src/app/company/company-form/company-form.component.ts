3cf6e691e3e9add2b730b6095745c2e12515787e
Almira Shaikh <almira.shaikh@1Rintern.local>
2022-10-12 17:33:52 +0530

    feat: company-list ui

--- src/app/company/company-form/company-form.component.ts
+++ src/app/company/company-form/company-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-company-form',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompanyFormComponent implements OnInit {
 
-  constructor() { }
+  public isAddMode: boolean;
+  private id!: string;
+
+  constructor(
+    private route: ActivatedRoute) 
+    { 
+    this.isAddMode = true;
+
+  }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.isAddMode = !this.id;
   }
 
 }
