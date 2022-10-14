08cf301321bc124d9f9825e4e225af3341df69e3
Almira Shaikh <almira.shaikh@1Rintern.local>
2022-10-14 10:33:42 +0530

    feat: company from crud

--- src/app/company/company.module.ts
+++ src/app/company/company.module.ts
@@ -6,6 +6,7 @@ import { CompanyComponent } from './company.component';
 import { CompanyListComponent } from './company-list/company-list.component';
 import { CompanyFormComponent } from './company-form/company-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     CompanyRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SharedModule
   ]
 })
 export class CompanyModule { }
--- src/app/shared/pipe/company.pipe.ts
+++ src/app/shared/pipe/company.pipe.ts
@@ -5,8 +5,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CompanyPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): string {
+    value = value.toUpperCase();
+    const companyName = value.split(' ');
+    const nameLogo = companyName[0].charAt(0) + companyName[1].charAt(0);
+    return nameLogo;
   }
 
 }
